1d3cef138b6e475f97885aa91e40173f013a39d6
Peter Huang <miterfrnats@gmail.com>
2020-02-14 19:56:32 +0800

    feat: move webpack.config.js to top level

--- auto/initialize-project-structure.js
+++ auto/initialize-project-structure.js
@@ -4,6 +4,8 @@ if (!fs.existsSync('src')) {
     fs.mkdirSync('src');
 }
 fs.copySync('./node_modules/swim/demo', './src/');
+fs.copySync('./node_modules/swim/demo/webpack.config.js', './webpack.config.js');
+fs.removeSync('./src/webpack.config.js');
 
 if (!fs.existsSync('dist')) {
     fs.mkdirSync('dist');
@@ -13,7 +15,7 @@ require('./update-core.js');
 
 const package = fs.readJSONSync('package.json');
 package.scripts['swim-update'] = 'npm install https://github.com/miterfrants/swim.git && node node_modules/swim/auto/update-core.js';
-package.scripts['swim-build'] = 'node node_modules/swim/auto/cacher-for-webpack.js && npx webpack --config ./src/webpack.config.js';
+package.scripts['swim-build'] = 'node node_modules/swim/auto/cacher-for-webpack.js && npx webpack --config ./webpack.config.js';
 fs.writeJSONSync('package.json', package, {
     spaces: 2,
     EOL: '\r\n'
